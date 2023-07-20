d077fb845eb2ee9e3692bf36ef221b098e204ebc
Ashelynne Juniper <ashe.juniper@pm.me>
2023-07-20 08:27:37 -0500

    Release v0.1.0-alpha.3
    + Remove return value from `launch_app`
    + Add function `sogouda_launch_app` to the C API

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,36 @@ extern crate web_view;
 // use deno_core::JsRuntime;
 use web_view::*;
 
-pub fn launch_app(title: &str, url: &str, width: Option<i32>, height: Option<i32>, is_frameless: Option<bool>, debug: Option<bool>) -> bool {
+unsafe extern "C" fn sogouda_launch_app(
+    title: *mut u8,
+    title_length: usize,
+    url: *mut u8,
+    url_length: usize,
+    width: i32,
+    height: i32,
+    is_frameless: bool,
+    debug: bool
+) {
+    let title_string =
+        String::from_raw_parts(
+            title,
+            title_length,
+            title_length
+        );
+
+    let url_string =
+        String::from_raw_parts(
+            url,
+            url_length,
+            url_length
+        );
+
     web_view::builder()
-        .title(title)
-        .content(Content::Url(url))
-        .size(width.unwrap_or(640), height.unwrap_or(480))
-        .frameless(is_frameless.unwrap_or(false))
-        .debug(debug.unwrap_or(false))
+        .title(title_string.as_str())
+        .content(Content::Url(url_string.as_str()))
+        .size(width, height)
+        .frameless(is_frameless)
+        .debug(debug)
         .user_data("")
         .invoke_handler(|webview, arg| {
             match arg {
@@ -20,9 +43,22 @@ pub fn launch_app(title: &str, url: &str, width: Option<i32>, height: Option<i32
             Ok(())
         })
         .run()
-        .unwrap();
+    .unwrap();
+}
 
-    return false;
+pub fn launch_app(title: &str, url: &str, width: Option<i32>, height: Option<i32>, is_frameless: Option<bool>, debug: Option<bool>) {
+    unsafe {
+        sogouda_launch_app(
+            title.to_string().as_mut_ptr(),
+            title.len(),
+            url.to_string().as_mut_ptr(),
+            url.len(),
+            width.unwrap_or(640),
+            height.unwrap_or(480),
+            is_frameless.unwrap_or(false),
+            debug.unwrap_or(false)
+        );
+    }
 }
 
 #[cfg(test)]
@@ -33,16 +69,18 @@ mod tests {
 
         const URL: &str = "https://ipfs.io/ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme";
 
-        let result =
-            launch_app(
-                "Gouda App",
-                URL,
-                None,
-                None,
-                None,
-                None
-            );
+        launch_app(
+            "Gouda App",
+            URL,
+            None,
+            None,
+            None,
+            None
+        );
 
-        assert_eq!(result, false);
+        assert_eq!(
+            true,
+            true
+        );
     }
 }
